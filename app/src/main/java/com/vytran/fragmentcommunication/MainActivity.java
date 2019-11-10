13913ad3de752e5f2c269949372f906f3af88280
vytran <vy.tran@7-11.com>
2019-11-09 20:21:28 -0600

    test 4

--- app/src/main/java/com/vytran/fragmentcommunication/MainActivity.java
+++ app/src/main/java/com/vytran/fragmentcommunication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements FragmentA.Fragmen
         System.out.println("Test for branch 1");
         System.out.println("Test 3");
         System.out.println("Test for branch 2");
-
+        System.out.println("Test for branch 3");
         
     }
 
